5a0f75c894fff4ba47a1cb11efe50b99de58ed1d
Alan Lamin <lamin.exe@gmail.com>
2020-10-21 11:44:31 -0300

    primeiro commit

---
+++ maketplace_l6/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::any('/Alan/{{id}}', function ($id) {
+    return "Alan - sua idade é {{id}}";
+});
